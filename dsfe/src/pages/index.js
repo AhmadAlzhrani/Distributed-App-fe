abc6511d5a55704be1f94dfb628c286345253858
Ahmad <roxel@Roxel.local>
2023-12-24 12:04:24 +0300

    completed

--- dsfe/src/pages/index.js
+++ dsfe/src/pages/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 function Home() {
     const [showUser, setShowUser] = useState(false);
@@ -9,12 +9,45 @@ function Home() {
     const [title, setName] = useState('');
     const [content, setId] = useState('');
 
+    const [ub, setUb] = useState(false);
+    const [ib, setIb] = useState(false);
+
+    useEffect(() => {
+        fetch('http://127.0.0.1:3001', {
+            method: 'GET',
+        })
+        .then(data => {
+            console.log('data',data);
+            if(data.status === 200){
+                setUb(true);
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+
+
+        fetch('http://localhost:80', {
+            method: 'GET',
+        })
+        .then(data => {
+            console.log(data);
+            if(data.status === 200){
+                setIb(true);
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+
+    }, []);
+
     const handleUserSubmit = (e) => {
         e.preventDefault();
         console.log({username, email});
-        
+
         // Send the form data to the REST API
-        fetch('https://api.example.com/endpoint', {
+        fetch('http://localhost:3001/users', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -37,9 +70,9 @@ function Home() {
 
     const handleItemSubmit = (e) => {
         e.preventDefault();
-        
+
         // Send the form data to the REST API
-        fetch('https://api.example.com/endpoint', {
+        fetch('http://localhost:80/items', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -49,7 +82,6 @@ function Home() {
                 content
             })
         })
-        .then(response => response.json())
         .then(data => {
             // Handle the response from the REST API
             console.log(data);
@@ -58,18 +90,23 @@ function Home() {
             // Handle any errors that occurred during the request
             console.error(error);
         });
+
     };
 
     return (
         <div className="flex columns-2 w-screen h-screen bg-gradient-to-l from-blue-950 from-10%" >
             <div className="flex flex-col bg-transparent shadow-lg rounded-lg ml-2 mb-2 w-[30vh] mt-96">
-                <button className='primary-button' onClick={() => setShowUser(!showUser)}>add user</button>
-                <button className='primary-button' onClick={() => setShowItem(!showItem)}>add item</button>
+                {ub && <div>
+                    <button className='primary-button' onClick={() => setShowUser(!showUser)}>add user</button>
+                </div>}
+                {ib && <div>
+                    <button className='primary-button' onClick={() => setShowItem(!showItem)}>add item</button>
+                </div>}
             </div>
             <div className="mx-auto my-auto">
                 <div className="">
                     {showUser && (
-                        <form className='boxx' onSubmit={handleUserSubmit}>
+                        <form id='userForm' className='boxx' onSubmit={handleUserSubmit}>
                             <div className=" px-4 py-2 space-x-8 ">
                                 <label htmlFor="user">Username</label>
                                 <input className='text-black' type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="AhmadAlzhrani" />
@@ -86,7 +123,7 @@ function Home() {
                 </div>
                 <div className="">
                     {showItem && (
-                        <form className='boxx' onSubmit={handleItemSubmit}>
+                        <form id='itemForm' className='boxx' onSubmit={handleItemSubmit}>
                             <div className=" px-4 py-2 space-x-8 ">
                                 <label htmlFor="user">Title</label>
                                 <input className='text-black' type="text" value={title} onChange={(e) => setName(e.target.value)} placeholder="box1" />
